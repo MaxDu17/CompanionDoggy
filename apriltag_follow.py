32784dba607982a801399377f803edbf64419077
Maximilian Du <maximilianjdu@gmail.com>
2025-02-17 22:49:03 +0000

    progress

--- apriltag_follow.py
+++ apriltag_follow.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import sys
 from dt_apriltags import Detector
+import imageio 
 
 import time
 from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
@@ -16,6 +17,9 @@ from unitree_sdk2py.go2.sport.sport_client import (
 import math
 import numpy as np 
 # from unitree_sdk2py.go2.robot_state.robot_state_client import RobotStateClient
+from datetime import datetime
+now = datetime.now() # current date and time
+date_time = now.strftime("%m_%d_%Y__%H_%M_%S")
 
 from collections import deque
 
@@ -36,17 +40,26 @@ if len(sys.argv)>1:
 else:
     ChannelFactoryInitialize(0)
 
+
+
+# import os
+# note to self: you have to run this outside of python first 
+# os.system("v4l2-ctl -d /dev/video0 --set-ctrl=exposure_auto=1")
+# os.system("v4l2-ctl -d /dev/video0 --set-ctrl=exposure_absolute=50")
+# this stuff sets up the camera 
+# remember that you need pip install imageio  and sudo apt-get install v4l-utils
+
 client = VideoClient()  # Create a video client
 client.SetTimeout(3.0)
 client.Init()
 
 # Open the default camera
 cam = cv2.VideoCapture(0)
-# Get the default frame width and height
 frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(frame_width)
 print(frame_height)
+
 
 sport_client = SportClient()  
 sport_client.SetTimeout(10.0)
@@ -54,6 +67,9 @@ sport_client.Init()
 
 remoteControl = RemoteHandler()
 remoteControl.Init()
+
+color_output = imageio.get_writer(f"videos/{date_time}.mp4", fps = 10)
+
 
 # position_queue = deque(maxlen = 3)
 
@@ -72,6 +88,7 @@ def get_onboard_camera_image():
 
     # Convert to numpy image
     image_data = np.frombuffer(bytes(data), dtype=np.uint8)
+    # print("reading")
     img = cv2.imdecode(image_data, cv2.IMREAD_GRAYSCALE)
     color_img = cv2.imdecode(image_data, cv2.IMREAD_COLOR) #cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     return color_img, True 
@@ -83,8 +100,13 @@ while True: # MAIN EXECUTION LOOP
         last_tag = None 
 
     # color_img, status = get_onboard_camera_image()
+    # try:
     status, color_img = cam.read()
     img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
+    # except:
+    #     print("skipping; bad read!")
+    #     continue 
+        
 
 
     cameraMatrix = np.load("calibration_matrix.npy")
@@ -98,16 +120,16 @@ while True: # MAIN EXECUTION LOOP
     # tags = at_detector.detect(img) # , True, camera_params, parameters['sample_test']['tag_size'])
 
 
-    if not HEADLESS_MODE:
-        for tag in tags:
-            for idx in range(len(tag.corners)):
-                cv2.line(color_img, tuple(tag.corners[idx-1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0), 10)
-            cv2.circle(color_img, (int(tag.center[0]), int(tag.center[1])), 20, (255, 0, 0), -1)
-            cv2.putText(color_img, str(tag.tag_id),
-                        org=(tag.corners[0, 0].astype(int)+10,tag.corners[0, 1].astype(int)+10),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=0.8,
-                        color=(0, 0, 255))
+    # if not HEADLESS_MODE:
+    for tag in tags:
+        for idx in range(len(tag.corners)):
+            cv2.line(color_img, tuple(tag.corners[idx-1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0), 10)
+        cv2.circle(color_img, (int(tag.center[0]), int(tag.center[1])), 20, (255, 0, 0), -1)
+        cv2.putText(color_img, str(tag.tag_id),
+                    org=(tag.corners[0, 0].astype(int)+10,tag.corners[0, 1].astype(int)+10),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.8,
+                    color=(0, 0, 255))
            
                 
     
@@ -117,7 +139,7 @@ while True: # MAIN EXECUTION LOOP
      
     if len(tags) == 0:
         last_tag = None 
-        # print("################ Tag not detected! ###########")
+        print("################ Tag not detected! ###########")
         # vanish_counter += 1 
         # if vanish_counter > 5:
         #     last_tag = None # reset if we haven't seen the tag in a moment 
@@ -131,24 +153,25 @@ while True: # MAIN EXECUTION LOOP
         velocity_error = position_error - last_error 
         integral_error += position_error 
         # turn position into velocity 
-        pd_error = position_error + 1 * velocity_error # + 0.05 * integral_error 
-        scaled_position_error = -np.clip(pd_error, -0.5, 0.5)
+        pd_error = position_error # + 1 * velocity_error # + 0.05 * integral_error 
+        scaled_position_error = -np.clip(pd_error, -1, 1)
         # print(pd_error)
         # print(velocity_error)
         # print(integral_error)
        
         distance_error = tag_location[2, 0] - 1.2
-        scaled_distance_error = np.clip(distance_error, -0.5, 0.5)
+        scaled_distance_error = np.clip(distance_error, -1.5, 2)
         last_error = position_error 
         # print(distance_error)
         
-        rotation = Rotation.from_matrix(last_tag.pose_R).as_euler('xyz') # , degrees = True)
-        yaw_error = -rotation[1] 
-        scaled_yaw_error = np.clip(yaw_error, -0.5, 0.5)
+        # rotation = Rotation.from_matrix(last_tag.pose_R).as_euler('xyz') # , degrees = True)
+        # yaw_error = -rotation[1] 
+        # scaled_yaw_error = np.clip(yaw_error, -0.5, 0.5)
         # print(yaw_error)
 
         
-        sport_client.Move(scaled_distance_error, scaled_position_error, yaw_error)
+        # sport_client.Move(scaled_distance_error, scaled_position_error, yaw_error)
+        sport_client.Move(scaled_distance_error, 0, scaled_position_error)
         # sport_client.Move(0, scaled_position_error, 0)
         # sport_client.Move(0, 0, yaw_error)
 
@@ -182,10 +205,12 @@ while True: # MAIN EXECUTION LOOP
         if not DEVELOP_MODE: 
             sport_client.Move(p_distance_signal, pd_signal,0)
         # sport_client.Move(pd_distance_signal, 0,0)
-        
-        
+    
+    color_output.append_data(cv2.cvtColor(color_img,cv2.COLOR_BGR2RGB))
+    
     if not HEADLESS_MODE: 
         cv2.imshow("Output", color_img)
         if cv2.waitKey(20) == 27:
             break
 
+color_output.close()
